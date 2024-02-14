aac6410d9c1d1d44b1e102739a9bb6e5385892eb
Guantol-Lemat <guantol.lemat@gmail.com>
2024-02-15 00:11:43 +0100

    Updated amazing_chest_ahead: - Removed Unnecessary Code - Randomness is now deterministic - Chest Morphs are now tied to Achievements - Additional Drops are now tied to Achievements - Added Compatibility with Chests Before Mother

--- resurrected_modpack/tweaks/amazing_chest_ahead.lua
+++ resurrected_modpack/tweaks/amazing_chest_ahead.lua
@@ -4,87 +4,64 @@ mod.CurrentModName = "Amazing Chest Ahead"
 
 local game = Game()
 
-local data
+local woodenChestPercent = 5
+local oldChestPercent = 5
 
---save/load
-
-function mod:onGameStart(con)
-	if not con then
-		data = {
-			foundItem = {},
-			check = false
-		}
-	else
-		local dataLoaded = Isaac.LoadModData(mod)
-		data = mod.json.decode(dataLoaded)
-	end
-end
-
-mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, mod.onGameStart)
-
-
-function mod:preGameExit(shouldSave)
-	if shouldSave then
-		Isaac.SaveModData(mod, mod.json.encode(data, "data"))
-	end
-end
-
-mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, mod.preGameExit)
-
+local crackedKeyPercent = 6.66
 
 --Chest Replacements
 
-function mod:chestReplace(pickup)
+local function IsNewChest(chest)
+	local sprite = chest:GetSprite()
+	return sprite:IsPlaying("Appear") or sprite:IsPlaying("AppearFast")
+end
+
+function mod:ChestReplace(pickup)
+	if not mod.Functions.IsAchievementUnlocked(Achievement.WOODEN_CHEST) then
+		return
+	end
 	local room = game:GetRoom()
 	local level = game:GetLevel()
 	local stage = level:GetStage()
-	if (pickup:GetSprite():IsPlaying("Appear") or pickup:GetSprite():IsPlaying("AppearFast")) and pickup:GetSprite():GetFrame() == 1 and room:GetType() ~= RoomType.ROOM_CHALLENGE and stage ~= 11 and not pickup:GetData().nomorph then
-		if pickup.Variant == 60 and math.random(100) <= 5 then
-			pickup:Morph(5, 55, 0, true, true, false)
-		elseif pickup.Variant == 50 and math.random(100) <= 5 then
-			pickup:Morph(5, 56, 0, true, true, false)
+	if not IsNewChest(pickup) then
+		return
+	end
+	if room:GetType() ~= RoomType.ROOM_CHALLENGE and stage ~= LevelStage.STAGE6 and not pickup:GetData().nomorph then
+		if pickup.InitSeed % (100 * 100) < (woodenChestPercent * 100) then
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_WOODENCHEST, mod.Enums.InitSubType.NO_MORPH, true, true, false)
 		end
 	end
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, mod.chestReplace)
+function mod:LockedChestReplace(pickup)
+	local room = game:GetRoom()
+	local level = game:GetLevel()
+	local stage = level:GetStage()
+	if not IsNewChest(pickup) then
+		return
+	end
+	if room:GetType() ~= RoomType.ROOM_CHALLENGE and stage ~= LevelStage.STAGE6 and not pickup:GetData().nomorph then
+		if pickup.InitSeed % (100 * 100) < (oldChestPercent * 100) then
+			pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_OLDCHEST, mod.Enums.InitSubType.NO_MORPH, true, true, false)
+		end
+	end
+end
+
+mod:AddPriorityCallback(ModCallbacks.MC_POST_PICKUP_INIT, CallbackPriority.LATER, mod.ChestReplace, PickupVariant.PICKUP_CHEST)
+mod:AddPriorityCallback(ModCallbacks.MC_POST_PICKUP_INIT, CallbackPriority.LATER, mod.LockedChestReplace, PickupVariant.PICKUP_LOCKEDCHEST)
 
 
 --Red Chest Extra Loot
 
-function mod:red(pickup)
+function mod:RedChestLoot(pickup)
+	if not mod.Functions.IsAchievementUnlocked(Achievement.RED_KEY) then
+		return
+	end
 	if pickup:GetSprite():IsPlaying("Open") and pickup:GetSprite():GetFrame() == 1 then
-		if math.random(5) == 1 then
-			Isaac.Spawn(5, 300, 78, pickup.Position, Vector.FromAngle(math.random(360)) * 5, nil)
+		if pickup.DropSeed % (100 * 100) < (crackedKeyPercent * 100) then
+			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TAROTCARD, Card.CARD_CRACKED_KEY, pickup.Position, Vector.FromAngle(math.random(360)) * 5, nil)
 		end
 	end
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, mod.red, PickupVariant.PICKUP_REDCHEST)
-
---Dupe Protection
-
-function mod:foundItemInit()
-	if not data then return end
-	if Game():GetLevel():GetCurrentRoomIndex() ~= Game():GetLevel():GetStartingRoomIndex() and not data.check then
-		for i = 1, 1000 do 
-			for y = 0, Game():GetNumPlayers() - 1 do
-				local player = Isaac.GetPlayer(y)
-				if player:HasCollectible(i) then table.insert(data.foundItem, i) end
-			end
-		end
-		data.check = true
-	end
-end
-
-mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.foundItemInit)
-
-
-function mod:foundItem(item)
-	if not data then return end
-	local unique = true
-	for i = 1, #data.foundItem do if item.SubType == data.foundItem[i] then unique = false end end
-	if unique == true then table.insert(data.foundItem, item.SubType) end
-end
-
-mod:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, mod.foundItem, 100)
+mod:AddCallback(ModCallbacks.MC_POST_PICKUP_UPDATE, mod.RedChestLoot, PickupVariant.PICKUP_REDCHEST)
