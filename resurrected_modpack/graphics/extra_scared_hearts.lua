10fb8cf02e8a39cd468d93a708ddeffb39c90038
Guantol-Lemat <guantol.lemat@gmail.com>
2025-07-21 14:10:25 +0200

    Rework Extra-Scared Hearts - Fixed Load problems. - Now makes use of a weighted pool system.

--- resurrected_modpack/graphics/extra_scared_hearts.lua
+++ resurrected_modpack/graphics/extra_scared_hearts.lua
@@ -1,113 +1,96 @@
 local TR_Manager = require("resurrected_modpack.manager")
 ExtraScaredHearts = TR_Manager:RegisterMod("Extra-Scared Hearts", 1)
 
---Modules
-require("esh_mcm");
-
---Settings
-ExtraScaredHearts.Settings = {
-    ["ignoreSpiderAnms"] = false,       --remove any scared hearts with spider legs
-};
-
-
 --MAIN FUNCTIONS--
 
-local numberOfAnms = 23;     --number of .anm2 files to choose from. .anm2 files should be named esh_1, esh_2, etc.
+--#region Extra Hearts Data
 
---Changes the scared heart's sprite
+---@enum ScaredHeartType
+local ScaredHeartType = {
+    SWEAT = 1,
+    FLY = 2,
+    VOID = 3,
+    SPIDER = 4,
+}
+
+local SCARED_HEARTS_ANIMATION_IDENTIFIERS = {
+    [ScaredHeartType.SWEAT] = "sweat",
+    [ScaredHeartType.FLY] = "fly",
+    [ScaredHeartType.VOID] = "void",
+    [ScaredHeartType.SPIDER] = "spider",
+}
+
+local DEFAULT_WEIGHTS = {
+    [ScaredHeartType.SWEAT] = 1.0,
+    [ScaredHeartType.FLY] = 1.0,
+    [ScaredHeartType.VOID] = 0.1,
+    [ScaredHeartType.SPIDER] = 1.0,
+}
+
+--#endregion
+
+--#region Pool
+
+local Pool = WeightedOutcomePicker()
+
+Pool:AddOutcomeFloat(ScaredHeartType.SWEAT, DEFAULT_WEIGHTS[ScaredHeartType.SWEAT])
+Pool:AddOutcomeFloat(ScaredHeartType.FLY, DEFAULT_WEIGHTS[ScaredHeartType.FLY])
+Pool:AddOutcomeFloat(ScaredHeartType.VOID, DEFAULT_WEIGHTS[ScaredHeartType.VOID])
+Pool:AddOutcomeFloat(ScaredHeartType.SPIDER, DEFAULT_WEIGHTS[ScaredHeartType.SPIDER])
+
+--#endregion
+
+---@param pickup EntityPickup
 function ExtraScaredHearts:ChangeAnm(pickup)
-    if pickup.SubType == HeartSubType.HEART_SCARED then
-        local sprite = pickup:GetSprite();
-        local animationToPlay = sprite:GetAnimation();
-        sprite:Load(ExtraScaredHearts:GetAnmFilepath(), true);
-        --play the animation that the scared heart was already playing
-        sprite:Play(animationToPlay);
+    if pickup.SubType ~= HeartSubType.HEART_SCARED then
+        return
     end
+
+    local anm2Path = ExtraScaredHearts:GetAnmFilepath(pickup.InitSeed)
+    local sprite = pickup:GetSprite();
+    local animationToPlay = sprite:GetAnimation();
+    sprite:Load(anm2Path, true);
+    --play the animation that the scared heart was already playing
+    sprite:Play(animationToPlay);
 end
 
 --/MAIN FUNCTIONS--
 
 --HELPER FUNCTIONS--
 
-local rng = RNG();
-local availableAnms = {};
-
 --Returns if the player is in the Void or not
-function ExtraScaredHearts:InVoid()
+local function in_void()
     return Game():GetLevel():GetAbsoluteStage() == LevelStage.STAGE7;
 end
 
---Creates a randomly ordered array of .anm2 filepaths
-function ExtraScaredHearts:SetupAvailableAnms()
-    --reset and populate the table
-    local anmsToPopulate = numberOfAnms;
-    if ExtraScaredHearts.Settings["ignoreSpiderAnms"] then        --option to remove any hearts with spider legs, which are the final two .anm2s
-        anmsToPopulate = anmsToPopulate - 2;
+---@param scaredHeartType ScaredHeartType
+---@return string
+local function build_scared_heart_path(scaredHeartType)
+    local animationId = SCARED_HEARTS_ANIMATION_IDENTIFIERS[scaredHeartType];
+    if not animationId then
+        error("Invalid scared heart type: " .. tostring(scaredHeartType));
     end
-    availableAnms = {};
-    for index = 1, anmsToPopulate do
-	    table.insert(availableAnms, "extrascaredhearts/esh_" .. index .. ".anm2");
-    end
-    --shuffle the table
-    for i = #availableAnms, 2, -1 do
-        local j = math.random(i);
-        availableAnms[i], availableAnms[j] = availableAnms[j], availableAnms[i];
-    end
+
+    return "extrascaredhearts/esh_" .. animationId .. ".anm2";
 end
 
---Returns a random .anm2 filepath that hasn't been used yet, or the Delirium .anm2 if in the Void
-function ExtraScaredHearts:GetAnmFilepath()
-    if ExtraScaredHearts:InVoid() then
-        return "extrascaredhearts/esh_21.anm2";
-    else
-        if #availableAnms == 0 then
-            ExtraScaredHearts:SetupAvailableAnms();
-        end
-        return table.remove(availableAnms);
+---@param seed integer
+---@return string
+function ExtraScaredHearts:GetAnmFilepath(seed)
+    if in_void() then
+        return build_scared_heart_path(ScaredHeartType.VOID);
     end
+
+    local rng = RNG(seed, 35)
+    local outcome = Pool:PickOutcome(rng);
+    return build_scared_heart_path(outcome);
 end
 
 --/HELPER FUNCTIONS--
-
-----SAVE DATA----
-
-local json = require("json");
-local SaveState = {};
-
---Saves the mod's data into a .dat file
-function ExtraScaredHearts:SaveModData(_, shouldSave)
-	--default values
-	SaveState["availableAnms"] = availableAnms;
-    SaveState["Settings"] = ExtraScaredHearts.Settings;
-    --SaveState["ignoreSpiderAnms"] = ExtraScaredHearts.Settings["ignoreSpiderAnms"];
-	--save the data
-	ExtraScaredHearts:SaveData(json.encode(SaveState));
-end
-
---Loads the mod's data from the .dat file
-function ExtraScaredHearts:LoadModData()
-	--only run this when there are no players initialized, so at the start of a run
-	local totalPlayers = #Isaac.FindByType(EntityType.ENTITY_PLAYER);
-	if totalPlayers == 0 then
-		--load the saved data into SaveState variable
-		if ExtraScaredHearts:HasData() then
-			SaveState = json.decode(ExtraScaredHearts:LoadData());
-			--update the local variables to match the saved variables
-			availableAnms = SaveState["availableAnms"] or availableAnms;
-            ExtraScaredHearts.Settings = SaveState.Settings or ExtraScaredHearts.Settings;
-		end
-	end
-end
-
-----/SAVE DATA----
 
 ----CALLBACKS----
 
 --Call the sprite function whenever a heart spawns (function will check if it is scared or not)
 ExtraScaredHearts:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, ExtraScaredHearts.ChangeAnm, PickupVariant.PICKUP_HEART)
---Save the mod's data before exiting the game
-ExtraScaredHearts:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, ExtraScaredHearts.SaveModData);
---Load the mod's data when starting the game (function only runs if there are 0 players initialized)
-ExtraScaredHearts:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, ExtraScaredHearts.LoadModData);
 
 ----/CALLBACKS----
