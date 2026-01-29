ea43d14d0069a15b09b5926a017536f77e963887
Guantol-Lemat <guantol.lemat@gmail.com>
2026-01-30 00:40:00 +0100

    Fix: fix bug in Expanded Blue Womb - fix a bug that granted access to the blue room multiple times.

--- resurrected_modpack/tweaks/expanded_blue_womb.lua
+++ resurrected_modpack/tweaks/expanded_blue_womb.lua
@@ -1,8 +1,9 @@
 local TR_Manager = require("resurrected_modpack.manager")
-
 local mod = TR_Manager:RegisterMod("Expanded Blue Womb", 1, true)
 
-local LEVEL = Game():GetLevel()
+local g_Game = Game()
+local g_Level = g_Game:GetLevel()
+
 local LOCKED_ROOM_TYPES = {
     [RoomType.ROOM_SHOP] = true,
     [RoomType.ROOM_TREASURE] = true,
@@ -15,41 +16,45 @@ local LOCKED_ROOM_TYPES = {
     [RoomType.ROOM_PLANETARIUM] = true
 }
 
-function mod:SetRedirectBlueWomb()
-    if LEVEL:GetStage() ~= LevelStage.STAGE4_3 then
+local function get_writable_room(rooms, i)
+    return rooms:Get(i)
+end
+
+if not REPENTOGON then
+    get_writable_room = function(rooms, i)
+        local roomDescriptor = rooms:Get(i)
+        local roomPtrHash = GetPtrHash(roomDescriptor)
+
+        for dimension = 0, 2 do
+            local writableRoomDescriptor = g_Level:GetRoomByIdx(roomDescriptor.SafeGridIndex, dimension)
+
+            if GetPtrHash(writableRoomDescriptor) == roomPtrHash then
+                return writableRoomDescriptor
+            end
+        end
+
+        return g_Level:GetRoomByIdx(roomDescriptor.SafeGridIndex, -1)
+    end
+end
+
+local function SetRedirectBlueWomb()
+    if g_Level:GetStage() ~= LevelStage.STAGE4_3 then
         return
     end
 
-    local rooms = LEVEL:GetRooms()
+    if g_Level:GetCurrentRoomIndex() ~= g_Level:GetStartingRoomIndex() or not g_Game:GetRoom():IsFirstVisit() then
+        return
+    end
+
+    local rooms = g_Level:GetRooms()
 
     for i = 0, rooms.Size-1 do
-        local roomDescriptor = rooms:Get(i)
-
+        local roomDescriptor = get_writable_room(rooms, i)
         if (roomDescriptor.VisitedCount == 0 and LOCKED_ROOM_TYPES[roomDescriptor.Data.Type]) then
             roomDescriptor.Flags = roomDescriptor.Flags | RoomDescriptor.FLAG_BLUE_REDIRECT
         end
     end
 end
 
-if REPENTOGON then
-    -- Using MC_POST_NEW_ROOM so that Glowing Hourglass shenanigans don't remove the flag prematurely
-    mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.SetRedirectBlueWomb)
-else
-    local enableOnUpdate = false
-
-    function mod:PostNewRoomWrapper()
-        if not pcall(mod.SetRedirectBlueWomb) then
-            enableOnUpdate = true
-        end
-    end
-
-    function mod:PostUpdateWrapper()
-        if enableOnUpdate then
-            enableOnUpdate = false
-            mod:SetRedirectBlueWomb()
-        end
-    end
-
-    mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.PostNewRoomWrapper)
-    mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.PostUpdateWrapper)
-end
+-- Using MC_POST_NEW_ROOM so that Glowing Hourglass shenanigans don't remove the flag prematurely
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, SetRedirectBlueWomb)
