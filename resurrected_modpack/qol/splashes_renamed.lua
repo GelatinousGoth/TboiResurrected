61819d160cd0fd9f5a5c163ca4a754f46ab6df04
Izzy Isakoi <upstreamkoifish@gmail.com>
2026-08-13 14:05:56 +1000

    Added: Extra FF splashes, FF China (And Items) resprites

--- resurrected_modpack/qol/splashes_renamed.lua
+++ resurrected_modpack/qol/splashes_renamed.lua
@@ -7,12 +7,17 @@ function mod:SplashesRenamed()
     if not FiendFolio then return end
         FiendFolio.SplashTexts = {
             Small = {
+			"enemies that don't look as isaac-y, but still look pretty good",
+            "it's a bug! it's a bug!",
+            "uninstall the game",
             "brought to you by the sweaty meta-fetishists"},
             Medium = {
             "the game just got harder!",
             "from the creators of antibirth!",
             "from the creators of repentance!",
             "we put the mental in experimental",
+			"beta version begone",
+            "edmund smiles upon you",
             "elder gods of the isaac community"},
             Large = {
             ":^)",
@@ -26,6 +31,10 @@ function mod:SplashesRenamed()
             "also try deliverance!",
             "also try retribution!",
             "also try alphabirth!",
+			"also try boss butch!",
+            "also try revelations!",
+            "also try fall from grace!",
+            "also try last judgement!",
             "it came from space",
             "limited edition",
             "the sequel!",
@@ -37,6 +46,15 @@ function mod:SplashesRenamed()
             "three days are remaining...",
             "now a homestuck mod",
             "now an everything mod",
+			"click to learn more.",
+            "someone should rework these foes",
+            "heaps of unused content",
+            "we hate fun!",
+            "everything has a price",
+            "too many fortunes!",
+            "Isaac fact!",
+            "powered by repentogon!",
+            "more than 500 enemies!",
             "beware of the pipeline"
             }
         }
