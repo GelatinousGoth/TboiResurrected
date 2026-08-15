d324c417e7dcda49d39c8aa4b225902d0eca6916
XxX_Lucia_Coding_Master_2007_XxX <82701672+luciagaming@users.noreply.github.com>
2026-08-15 14:07:49 +0200

    New tear color for FF China (old was default)

--- resurrected_modpack/tweaks/ff_china_new_tears.lua
+++ resurrected_modpack/tweaks/ff_china_new_tears.lua
@@ -1,9 +1,10 @@
 local TR_Manager = require("resurrected_modpack.manager")
 
 local mod = TR_Manager:RegisterMod("FF China's New Tears", 1)
-if not FiendFolio then return end
+
 function mod:changeChinaTears()
-    FiendFolio.ColorChinaYellow = Color(1,1,1,1,0,0,0)
-	FiendFolio.ColorChinaYellow:SetColorize(0,0,0,0)
+if not FiendFolio then return end
+    FiendFolio.ColorChinaYellow = Color(0.93,0.87,1.0,1.0,0.0,0.0,0.0)
+	FiendFolio.ColorChinaYellow:SetColorize(0.55,0.18,0.77,0.22)
 end
 mod:AddCallback(ModCallbacks.MC_POST_MODS_LOADED, mod.changeChinaTears)
